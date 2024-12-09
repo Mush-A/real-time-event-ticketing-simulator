38fb3550505c82e9910af9f4acb2806c8cddd83a
Musharaf <musharafa@sunstonels.com>
2024-12-09 14:24:38 +0530

    Add: remove old component

--- fe-client/src/app/app.routes.ts
+++ fe-client/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
-// import {SimulationComponent} from './ui/simulation/simulation.component';
 import {SimulationComponent} from './ui/simulator/simulation/simulation.component';
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
-  // { path: "simulation", component: SimulationComponent },
-  { path: "new-simulation", component: SimulationComponent },
+  { path: "simulation", component: SimulationComponent },
 ];
